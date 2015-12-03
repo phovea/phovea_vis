36ef39aa1a6ffb5629e53de6a4961a0176c75950
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-12-03 10:15:31 +0100

    switching corner case for selection

--- distribution.js
+++ distribution.js
@@ -93,6 +93,9 @@ define(['exports', 'd3', '../caleydo_core/main', '../caleydo_core/idtype', '../c
     var yscale = that.yscale = d3.scale.linear().range([0, size[1]]);
 
     var l = function (event, type, selected) {
+      if (!that.hist_data) {
+        return;
+      }
       var highlights = that.hist_data.map(function (entry, i) {
         var s = entry.range.intersect(selected);
         return {

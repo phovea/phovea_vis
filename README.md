026ba335db3aff94580714bf791983d0d3da2348
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-02-26 09:27:00 +0100

    merge repositories (graph->core, vis_graph->vis, provenance->clue, tooltip->d3)

--- README.md
+++ README.md
@@ -1,5 +1,58 @@
 Caleydo Vis
-=====================
+===========
 
-a set of standard visualization techniques
+Purpose
+-------
+This plugins provides a set of standard visualizations for different datatypes. 
+
+Including
+
+Axis
+ * extension id: `axis`
+ * module: `axis.ts`
+ * accepts: (real|int) vectors
+
+Bar Plot
+ * extension id: `barplot`
+ * module: `barplot.ts`
+ * accepts: (real|int) vectors
+
+Box Plot
+ * extension id: `caleydo-vis-box`
+ * module: `box.ts`
+ * accepts: (real|int) vectors
+
+Kaplan Meier Plot 
+ * extension id: `caleydo-vis-kaplanmeier`
+ * module: `kaplanmeier.ts`
+ * accepts: int vectors
+
+Table
+ * extension id: `table`
+ * module: `table.ts`
+ * accepts: matrices, tables, and vectors
+ 
+Scatterplot
+ * extension id: `scatterplot`
+ * module: `scatterplot.ts`
+ * accepts: matrices
+
+Histogram 
+ * extension id: `caleydo-vis-histogram`
+ * module: `distribution.ts`
+ * accepts: (categorical|real|int) (vectors|matrix|stratification)
+
+1D HeatMap (caleydo-vis-heatmap1d
+
+TODO
+ * Mosaic (caleydo-vis-mosaic)
+
+
+Installation
+------------
+```bash
+./manage.sh clone Caleydo/bundle_web
+```
+
+
 
---
+++ force_directed_graph.ts
@@ -0,0 +1,174 @@
+/**
+ * Created by Samuel Gratzl on 05.08.2014.
+ */
+/// <reference path="../../tsd.d.ts" />
+//depend on a css dependency
+/// <amd-dependency path='css!./style' />
+
+/* global define */
+'use strict';
+
+import d3 = require('d3');
+import vis = require('../caleydo_core/vis');
+import graph = require('../caleydo_core/graph');
+import C = require('../caleydo_core/main');
+
+export class ForceDirectedGraphVis extends vis.AVisInstance implements vis.IVisInstance {
+  private $node:d3.Selection<any>;
+
+  private options = {
+    scale: [1, 1],
+    rotate: 0,
+    colors: true
+  };
+
+  constructor(public data:graph.GraphProxy, public parent:Element, options: any) {
+    super();
+    C.mixin(this.options, options);
+    this.$node = this.build(d3.select(parent));
+    this.$node.datum(data);
+    vis.assignVis(<Element>this.$node.node(), this);
+  }
+
+  /**
+   * the raw size without any scaling factors applied
+   * @returns {any[]}
+   */
+  get rawSize(): [number, number] {
+    return [300,300];
+  }
+
+  /**
+   * access to the HTML Element of this visualization
+   * @returns {Element}
+   */
+  get node() {
+    return <Element>this.$node.node();
+  }
+
+  /**
+   * get/set an option of this vis
+   * @param name
+   * @param val
+   * @returns {any}
+   */
+  option(name: string, val? : any) {
+    if (arguments.length === 1) {
+      return this.options[name];
+    } else {
+      this.fire('option', name, val, this.options[name]);
+      this.fire('option.'+name, val, this.options[name]);
+      this.options[name] = val;
+      //handle option change
+    }
+  }
+
+  /**
+   * transform this visualization given the scaling and rotation factor
+   * @param scale a two number array
+   * @param rotate a factor in degree
+   * @returns {any}
+   */
+  transform(scale?: number[], rotate: number = 0) {
+    var bak = {
+      scale: this.options.scale || [1,1],
+      rotate: this.options.rotate || 0
+    };
+    if (arguments.length === 0) {
+      return bak;
+    }
+    var raw = this.rawSize;
+    this.$node.style('transform','rotate('+rotate+'deg)');
+    this.$node.attr('width', raw[0] * scale[0]).attr('height',raw[1] * scale[1]);
+    this.$node.select('g').attr('transform','scale('+scale[0]+','+scale[1]+')');
+    var new_ = {
+      scale: scale,
+      rotate: rotate
+    };
+    this.fire('transform',new_, bak);
+    this.options.scale = scale;
+    this.options.rotate = rotate;
+    return new_;
+  }
+
+  private build($parent:d3.Selection<any>) {
+    //create a svg and append a group for the transform
+    var scaleFactor = this.options.scale, size = this.size;
+
+    //create the svg
+    var $svg = $parent.append('svg').attr({
+      width: size[0],
+      height: size[1],
+      'class': 'graph-force'
+    });
+    var $root = $svg.append('g').attr('transform','scale('+scaleFactor[0]+','+scaleFactor[1]+')');
+
+    var colors = d3.scale.category20().range();
+
+    this.data.impl().then((graph) => {
+      const nodes = graph.nodes.map((n) => ({ v : n }));
+      const lookup = d3.map(nodes, (d) => String(d.v.id));
+      const edges = graph.edges.map((n) => ({ v : n, source: lookup.get(String(n.source.id)), target: lookup.get(String(n.target.id)) }));
+      const f = d3.layout.force()
+        .size(this.rawSize);
+      f.nodes(nodes).links(edges);
+
+      const $links = $root.selectAll('.edge').data(edges);
+      $links.enter().append('line').classed('edge',true);
+      $links.attr('data-type',(d) => d.v.type);
+
+      $links.exit().remove();
+      const $nodes = $root.selectAll('.node').data(nodes);
+      $nodes.enter().append('circle')
+        .classed('node',true).attr('r',5)
+        .call(f.drag)
+        .on('click', (d) => {
+          console.log(d.v);
+        })
+        .append('title');
+      $nodes.attr('data-type',(d) => d.v.type);
+
+      if (this.options.colors) {
+        $links.style('stroke', (d) => {
+          var c = lookup[d.v.type];
+          if (!c) {
+            c = colors.shift();
+            lookup[d.v.type] = c;
+          }
+          return c;
+        });
+        $nodes.style('fill', (d) => {
+          var c = lookup[d.v.type];
+          if (!c) {
+            c = colors.shift();
+            lookup[d.v.type] = c;
+          }
+          return c;
+        });
+      }
+
+      $nodes.select('title').text((d) => d.v.id+' '+d.v.type);
+      f.on('tick', () => {
+        $links.attr({
+          x1: (d) => (<any>d.source).x,
+          y1: (d) => (<any>d.source).y,
+          x2: (d) => (<any>d.target).x,
+          y2: (d) => (<any>d.target).y
+        });
+
+        $nodes.attr({
+          cx: (d) => (<any>d).x,
+          cy: (d) => (<any>d).y
+        });
+      });
+
+      this.markReady();
+      f.start();
+    });
+    return $svg;
+  }
+}
+
+export function create(data:graph.GraphProxy, parent:Element, options) {
+  return new ForceDirectedGraphVis(data, parent, options);
+}

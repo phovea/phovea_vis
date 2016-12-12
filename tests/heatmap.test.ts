11d258372db64daa287a30b1f912c0d924bac79b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-12-12 14:49:12 -0500

    Make module methods more visible so we can test them.

--- tests/heatmap.test.ts
+++ tests/heatmap.test.ts
@@ -1,8 +1,15 @@
-import {toScale} from '../src/heatmap';
+import {toScale, defaultDomain, defaultColor} from '../src/heatmap';
 
 describe('toScale', () => {
   it('is method', () => {
     expect(typeof toScale).toEqual('function');
   });
-  // TODO
+  it('produces full gamut', () => {
+    var value = null; // TODO
+    var domain = defaultDomain(value);
+    var range = defaultColor(value);
+    var scale = toScale(value).domain(domain).range(range);
+    expect(scale(-1)).toEqual('#00F');
+    expect(scale(1)).toEqual('#FFF');
+  });
 });

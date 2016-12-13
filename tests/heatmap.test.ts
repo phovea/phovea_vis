bb9b7b5025a3651d0979189865f3e6737c315b3f
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2016-12-13 15:21:01 -0500

    Add tests

--- tests/heatmap.test.ts
+++ tests/heatmap.test.ts
@@ -1,15 +1,52 @@
 import {toScale, defaultDomain, defaultColor} from '../src/heatmap';
 
 describe('toScale', () => {
-  it('is method', () => {
-    expect(typeof toScale).toEqual('function');
+  it('handles negative and positive', () => {
+    const value = {
+      type: 'real',
+      range: [-1, 1]
+    };
+    const domain = defaultDomain(value);
+    const range = defaultColor(value);
+    const scale = toScale(value).domain(domain).range(range);
+    expect(scale(-1)).toEqual('#0000ff');
+    expect(scale(0)).toEqual('#ffffff');
+    expect(scale(1)).toEqual('#ff0000');
   });
-  it('produces full gamut', () => {
-    var value = null; // TODO
-    var domain = defaultDomain(value);
-    var range = defaultColor(value);
-    var scale = toScale(value).domain(domain).range(range);
-    expect(scale(-1)).toEqual('#00F');
-    expect(scale(1)).toEqual('#FFF');
+  it('handles skewed data', () => {
+    const value = {
+      type: 'real',
+      range: [-0.1, 10]
+    };
+    const domain = defaultDomain(value);
+    const range = defaultColor(value);
+    const scale = toScale(value).domain(domain).range(range);
+    expect(scale(-0.1)).toEqual('#fcfcff');
+    expect(scale(0)).toEqual('#ffffff');
+    expect(scale(5)).toEqual('#ff8080');
+    expect(scale(10)).toEqual('#ff0000');
+  });
+  it('handles positive', () => {
+    const value = {
+      type: 'real',
+      range: [0, 1]
+    };
+    const domain = defaultDomain(value);
+    const range = defaultColor(value);
+    const scale = toScale(value).domain(domain).range(range);
+    expect(scale(0)).toEqual('#ffffff');
+    expect(scale(0.5)).toEqual('#ff8080'); // interpolation
+    expect(scale(1)).toEqual('#ff0000');
+  });
+  it('handles negative', () => {
+    const value = {
+      type: 'real',
+      range: [-1, 0]
+    };
+    const domain = defaultDomain(value);
+    const range = defaultColor(value);
+    const scale = toScale(value).domain(domain).range(range);
+    expect(scale(-1)).toEqual('#ffffff'); // TODO: Should be #0000ff?
+    expect(scale(0)).toEqual('#ff0000'); // TODO: Should be #ffffff?
   });
 });

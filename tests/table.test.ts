c2897eb3e1378cd646a12435683fc5eed8743ec6
mccalluc <mccalluc@users.noreply.github.com>
2016-12-19 09:27:48 -0500

    Mccalluc/table headers should not be coupled (#18)
    * Checkpoint: Need to figure out how to create an HTMLElement in context of a test
    
    * Closer, but the table does not have any rows or columns
    
    * Make lint happy. Still not reproducing the Travis error locally
    
    * Might need a newer browser to run tests? "49" is fairly arbitrary.
    
    * needed to wait for render. TODO: test bug behavior
    
    * Tests pass locally, but the behavior needs to be fixed
    
    * Consistent string breaks / put it all in one "it"
    
    * The test I would like to write, but cant, because of project structure

---
+++ tests/table.test.ts
@@ -0,0 +1,63 @@
+import {create} from '../src/table';
+import {parseMatrix} from 'phovea_d3/src/parser';
+import {parse} from 'phovea_core/src/range';
+
+describe('table', () => {
+  var square_table;
+  var col_table;
+  var row_table;
+  beforeEach(function(done) {
+    // Yes, this is redundant, but it's ok for now.
+    const data = parseMatrix(
+        [ // raw data as 2D array
+          [1, 2],
+          [3, 4]
+        ],
+        ['X', 'Y'], // row_ids
+        ['A', 'B'] // col_ids
+    );
+    const element = document.createElement('div');
+    square_table = create(data, element);
+    col_table = create(data.view(parse([[0,1],[1]])), element);
+    row_table = create(data.view(parse([[1],[0,1]])), element);
+    setTimeout(function() {
+      done();
+    }, 1000); // TODO: Can we do better than this? create() doesn't let us provide a callback?
+  });
+  it('renders', () => {
+    expect(square_table.node.innerHTML).toEqual(
+        '<thead><tr><th>ID</th><th>A</th><th>B</th></tr></thead>'
+        +'<tbody>'
+        +'<tr><th>X</th><td>1</td><td>2</td></tr><tr>'
+        +'<th>Y</th><td>3</td><td>4</td></tr>'
+        +'</tbody>');
+    expect(col_table.node.innerHTML).toEqual(
+        '<thead><tr><th>ID</th><th>A</th><th>B</th></tr></thead>' // TODO: Should just be "B", right?
+        +'<tbody>'
+        +'<tr><th>X</th><td>2</td></tr>'
+        +'<tr><th>Y</th><td>4</td></tr>'
+        +'</tbody>');
+    expect(row_table.node.innerHTML).toEqual(
+        '<thead><tr><th>ID</th><th>B</th></tr></thead>' // TODO: Should be "A" + "B", right?
+        +'<tbody>'
+        +'<tr><th>Y</th><td>3</td><td>4</td></tr>'
+        +'</tbody>');
+  });
+});
+
+// TODO
+// describe('MatrixView', () => {
+//   it('sub-selects columns', () => {
+//     const matrix = parseMatrix(
+//         [ // raw data as 2D array
+//           [1, 2],
+//           [3, 4]
+//         ],
+//         ['X', 'Y'], // row_ids
+//         ['A', 'B'] // col_ids
+//     );
+//     const col_range = parse([[0,1],[1]]);
+//     const view = new MatrixView(matrix, col_range);
+//     expect(view.cols()).toEqual("foo");
+//   })
+// });

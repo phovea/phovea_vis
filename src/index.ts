d4806a21baafcbbbc1dec4a149951034b7cb5ce0
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 10:58:18 +0200

    add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './distribution';
+export * from './heatmap';
+export * from './list';
+
+export * from './axis';
+export {default as Axis} from './axis';
+export * from './box';
+export {default as BoxPlot} from './box';
+export * from './force_directed_graph';
+export * from './kaplanmeier';
+export * from './table';

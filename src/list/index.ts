4148503cc200f49131b459068914f4c1eb8bfe91
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 10:52:56 +0200

    add src/list/index.ts

---
+++ src/list/index.ts
@@ -0,0 +1,7 @@
+export * from './internal/internal';
+export * from './barplot';
+export {default as BarPlot} from './barplot';
+export * from './list';
+export {default as List} from './list';
+export * from './proportionalSymbol';
+export {default as ProportionalSymbol} from './proportionalSymbol';

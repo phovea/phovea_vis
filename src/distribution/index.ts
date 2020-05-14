a606c0d6dd0f8af6db7069f163858b049ed6d09e
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 10:37:53 +0200

    fix src/distribution/index.ts

--- src/distribution/index.ts
+++ src/distribution/index.ts
@@ -2,6 +2,10 @@
  * Created by Samuel Gratzl on 26.01.2016.
  */
 
-export {default as Histogram, create} from './Histogram';
-export {default as Mosaic, create as createMosaic} from './Mosaic';
-export {default as Pie, create as createPie} from './Pie';
+export * from './HistData';
+export * from './Histogram';
+export {default as Histogram} from './Histogram';
+export * from './Mosaic';
+export {default as Mosaic} from './Mosaic';
+export * from './Pie';
+export {default as Pie} from './Pie';

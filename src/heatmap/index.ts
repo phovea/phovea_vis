4ba6fe1d814cb3b9d617e49d30785eecd7cc6d3f
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-14 10:52:18 +0200

    add src/heatmap/index.ts

--- src/heatmap/index.ts
+++ src/heatmap/index.ts
@@ -2,5 +2,19 @@
  * Created by Samuel Gratzl on 05.08.2014.
  */
 
-
-
+export * from './defaultUtils';
+export * from './HeatMap';
+export {default as HeatMap} from './HeatMap';
+export * from './HeatMap1D';
+export {default as HeatMap1D} from './HeatMap1D';
+export * from './IHeatMapRenderer';
+export * from './IScale';
+export * from './AHeatMapCanvasRenderer';
+export {default as AHeatMapCanvasRenderer} from './AHeatMapCanvasRenderer';
+export * from './HeatMapCanvasRenderer';
+export {default as HeatMapCanvasRenderer} from './HeatMapCanvasRenderer';
+export * from './HeatMapDOMRenderer';
+export {default as HeatMapDOMRenderer} from './HeatMapDOMRenderer';
+export * from './HeatMapImageRenderer';
+export {default as HeatMapImageRenderer} from './HeatMapImageRenderer';
+export * from './ICommonHeatMapOptions';
